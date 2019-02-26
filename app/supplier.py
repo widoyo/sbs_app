918fd17966d843b9e51e62a52a8b8efe28a81388
Widoyo <widoyo@gmail.com>
2019-02-26 10:56:52 +0700

    MEnambah Form add supplier

--- app/supplier.py
+++ app/supplier.py
@@ -1,9 +1,11 @@
 '''Sales App
 '''
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required
+from sqlalchemy import desc
 
 from app.models import Supplier
+from app import db
 
 bp = Blueprint('supplier', __name__)
 
@@ -12,3 +14,25 @@ bp = Blueprint('supplier', __name__)
 def index():
     supplier = Supplier.query.all()
     return render_template('supplier/index.html', supplier=supplier)
+
+
+@bp.route('/<kodesup>')
+@login_required
+def show(kodesup):
+    supplier = Supplier.query.filter(Supplier.KODESUP == kodesup).first()
+    return render_template('supplier/show.html', supplier=supplier)
+
+
+@bp.route('/add', methods=('GET', 'POST'))
+@login_required
+def add():
+    if request.method == 'POST':
+        sup = Supplier(NAMA=request.form['nama'], KODESUP=get_last_kode_sup())
+        db.session.add(sup)
+        db.session.commit()
+    return render_template('supplier/add.html')
+
+
+def get_last_kode_sup():
+    last_sup = db.session.query(Supplier).order_by(desc(Supplier.KODESUP)).first()
+    return int(last_sup.KODESUP) + 1
